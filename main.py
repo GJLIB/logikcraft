4b7b78b7915df6dc144a919429e29c48fc18f6b0
GJIIB <glebnazarov.2008@gmail.com>
2025-01-30 20:54:56 +0200

    + нові зміни в гравця та генерація дерев

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ app = Ursina()
 
 from models import*
 
-
-
 sky = Sky()
 map = Map()
-map.new_map()
+map.new_map(size = 30)
 
-
-
-player = Player()
+player = Player(speed = 5, jump_height = 3)
+window.fullscreen = True
 
 app.run()
--- models.py
+++ models.py
@@ -4,17 +4,32 @@ from ursina.shaders import basic_lighting_shader
 from numpy import floor
 import os
 from ursina.prefabs.first_person_controller import FirstPersonController
+import random
 
 block_textures = []
 BASE_DIR = os.getcwd()
 BLOCKS_DIR = os.path.join(BASE_DIR, 'assets/blocks')
 file_list = os.listdir(BLOCKS_DIR)
+print(file_list)
 
 for image in file_list:
     texture = load_texture('assets/blocks' + os.sep + image)
     block_textures.append(texture)
 
 
+class Tree(Entity):
+    def __init__(self, pos, **kwargs):
+        super().__init__(
+            parent = scene, 
+            model = 'assets/texture_tree/scene.gltf',
+            position = pos,
+            scale = 5,
+            collider = 'box',
+            origin_y = 0.5,
+            color = color.color(0, 0, random.uniform(0.9, 1)),
+            shader = basic_lighting_shader,
+            **kwargs
+        )
 
 class Block(Entity):
     def __init__(self, block_type, pos, **kwargs):
@@ -44,15 +59,50 @@ class Map(Entity):
                 y = floor(self.noise([x/24, z/24])*6)
                 block = Block(4, (x, y, z))
 
+                random_num = random.randint(1, 100)
+                if rand_num == 71:
+                    Tree((x, y+1, z))
+
+
 class Player(FirstPersonController):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.hand_block = Entity(parent = camera.ui, model = 'cube',
+        texture = block_textures[Block.current], scale = 0.2, position=(0.6, -0.42),
+        shader=basic_lightning_shader, rotation = Vec3(30, -30, 10))
 
     def input(self, key):
         super().input(key)
+
+        if key == "scroll up":
+            Block.current +=1
+            if Block.current >= len(block_textures):
+                Block.current = 0
+            self.hand_block.texture = block_textures[Bock.current]
+
+        if key == "scroll down":
+            Block.current -=1
+            if Block.current < 0:
+                Block.current = len(block_textures) - 1
+            self.hand_block.texture = block_textures[Bock.current]
+               
+
         if key == 'left mouse down' and mouse.hovered_entity:
             destroy(mouse.hovered_entity)
         if key == 'right mouse down' and mouse.hovered_entity:
             hit_info = raycast(camera.world_position, camera.forward, distance = 15)
             if hit_info.hit:
                 Block(1, hit_info.entity.position + hit_info.normal)
+
+    def update(self):
+        super().update()
+        if held_keys['control']:
+            self.speed = 10
+        else:
+            self.speed = 5
+        
+        if held_keys['shift']:
+            self.speed = 3
+            self.height = 1
+        else:
+            self.height = 2
